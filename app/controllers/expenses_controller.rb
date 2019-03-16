8351fd566b2d7273f1a20a5c094556f20909f459
Eric Suescun <edsuescun@gmail.com>
2019-03-16 13:41:51 -0500

    Se implementa el filtro como lo tienen en Makeitreal

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,19 +3,34 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
-    @userExpenses = Expense.where(["user_id = ?", current_user])
-
-  	if (params[:concept] == '')
-  		@expenses = @userExpenses.where(["category_id = ?", params[:category_id]])
-  	end
-
-  	if (params[:category_id] == '')
-  		@expenses = @userExpenses.where('concept LIKE ?', "\%" + params[:concept].to_s + "\%" ).all
-  	else
-  		@expenses = @userExpenses.where(["category_id = ?", params[:category_id]])
-  		@expenses = @expenses.where('concept LIKE ?', "\%" + params[:concept].to_s + "\%" )
-  	end
-
+      @expenses = Expense.where(["user_id = ?", current_user])
+      if params[:concept].present?
+        @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+      end
+      if params[:category_id].present?
+        @expenses = @expenses.where("category_id = ?", params[:category_id])
+      end
   end
+
 end
+
+
+
+
+
+    # def index
+
+    #   @userExpenses = Expense.where(["user_id = ?", current_user])
+
+    #   if (params[:concept] == '')
+    #     @expenses = @userExpenses.where(["category_id = ?", params[:category_id]])
+    #   end
+
+    #   if (params[:category_id] == '')
+    #     @expenses = @userExpenses.where('concept LIKE ?', "\%" + params[:concept].to_s + "\%" ).all
+    #   else
+    #     @expenses = @userExpenses.where(["category_id = ?", params[:category_id]])
+    #     @expenses = @expenses.where('concept LIKE ?', "\%" + params[:concept].to_s + "\%" )
+    #   end
+
+    # end
