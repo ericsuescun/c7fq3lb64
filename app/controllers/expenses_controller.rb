29d4b62e6be21abbb32ceffa8dd4d60217b71d0d
Eric Suescun <edsuescun@gmail.com>
2019-03-15 19:01:43 -0500

    Se implementa la logica de query

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,18 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+  	@expenses = Expense.all
+
+  	if (params[:concept] == '')
+  		@expenses = Expense.where(["category_id = ?", params[:category_id]])
+  	end
+
+  	if (params[:category_id] == '')
+  		@expenses = Expense.where('concept LIKE ?', "\%" + params[:concept].to_s + "\%" ).all
+  	else
+  		@expenses = Expense.where(["category_id = ?", params[:category_id]])
+  		@expenses = @expenses.where('concept LIKE ?', "\%" + params[:concept].to_s + "\%" )
+  	end
+
   end
 end
