db00d1b0c485c92cc57b93358176043fd54c9525
Eric Suescun <edsuescun@gmail.com>
2019-03-15 23:36:34 -0500

    Se crean dos usuarios y se populan en la DB de Expese

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190316033242) do
+ActiveRecord::Schema.define(version: 20190316041659) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20190316033242) do
     t.decimal  "amount"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
